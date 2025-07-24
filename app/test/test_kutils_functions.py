2e621a2d6021a6b3250cabf5a18ccca3a86b1416
astratakis <andrew.stratakis@gmail.com>
2025-07-24 14:41:49 +0000

    minor change in test

--- app/test/test_kutils_functions.py
+++ app/test/test_kutils_functions.py
@@ -1,6 +1,16 @@
 from kutils.functions import *
 
 import pytest
+import os
+
+config = {
+    "KEYCLOAK_REALM": os.getenv("KEYCLOAK_REALM", None),
+    "KEYCLOAK_URL": os.getenv("KEYCLOAK_URL", None),
+    "KEYCLOAK_CLIENT_ID": os.getenv("KEYCLOAK_CLIENT_ID", None),
+    "KEYCLOAK_CLIENT_SECRET": os.getenv("KEYCLOAK_CLIENT_SECRET", None)
+}
 
 def test_initialize_admin_client():
-    assert 1
+    
+    kcadmin = initialize_admin_client(config["KEYCLOAK_URL"], config["KEYCLOAK_REALM"], config["KEYCLOAK_CLIENT_ID"], config["KEYCLOAK_CLIENT_SECRET"])
+    assert kcadmin is not None
