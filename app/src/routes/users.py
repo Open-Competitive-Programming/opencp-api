2880ebc97b65dc481ac3ed6fd83a8730f3919266
astratakis <andrew.stratakis@gmail.com>
2025-07-24 17:14:18 +0000

    formatted using black

--- app/src/routes/users.py
+++ app/src/routes/users.py
@@ -1,8 +1,15 @@
-from flask import request, jsonify, current_app, url_for
 from apiflask import APIBlueprint
-import requests
-import logging 
-import xml.etree.ElementTree as ET
-import kutils
 
-users_bp = APIBlueprint('users_blueprint', __name__, tag={'name':'User Management','description':'Operations related to management of users (CRUD, Authentication, Authorization)'})
+users_bp = APIBlueprint(
+    "users_blueprint",
+    __name__,
+    tag={
+        "name": "User Management",
+        "description": "Operations related to management of users (CRUD, Authentication, Authorization)",
+    },
+)
+
+
+@users_bp.route("/api/v1/users")
+def api_get_users():
+    pass
--- app/test/test_kutils_functions.py
+++ app/test/test_kutils_functions.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 
 from kutils.functions import *
 
@@ -7,10 +6,16 @@ config = {
     "KEYCLOAK_REALM": os.getenv("KEYCLOAK_REALM", None),
     "KEYCLOAK_URL": os.getenv("KEYCLOAK_URL", None),
     "KEYCLOAK_CLIENT_ID": os.getenv("KEYCLOAK_CLIENT_ID", None),
-    "KEYCLOAK_CLIENT_SECRET": os.getenv("KEYCLOAK_CLIENT_SECRET", None)
+    "KEYCLOAK_CLIENT_SECRET": os.getenv("KEYCLOAK_CLIENT_SECRET", None),
 }
 
+
 def test_initialize_admin_client():
-    
-    kcadmin = initialize_admin_client(config["KEYCLOAK_URL"], config["KEYCLOAK_REALM"], config["KEYCLOAK_CLIENT_ID"], config["KEYCLOAK_CLIENT_SECRET"])
+
+    kcadmin = initialize_admin_client(
+        config["KEYCLOAK_URL"],
+        config["KEYCLOAK_REALM"],
+        config["KEYCLOAK_CLIENT_ID"],
+        config["KEYCLOAK_CLIENT_SECRET"],
+    )
     assert kcadmin is not None
