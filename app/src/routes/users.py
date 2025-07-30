6b3cadeb105dac5229abb14201f165ca060f785b
astratakis <andrew.stratakis@gmail.com>
2025-07-30 22:30:19 +0000

    minor changes to make pylint happier

--- app/src/routes/users.py
+++ app/src/routes/users.py
@@ -1,27 +1,40 @@
-from apiflask import APIBlueprint
-from flask import g, request
+"""
+Documentation
+"""
 
 import uuid
 import schema
 import datetime
 
+from apiflask import APIBlueprint
+from flask import g, request
+
+
 users = APIBlueprint(
-    "users_blueprint",
+    "users",
     __name__,
     tag={
         "name": "User Management",
-        "description": "Operations related to management of users (CRUD, Authentication, Authorization)",
+        "description": "Operations related to management of users (CRUD, Auth)",
     },
 )
 
 
 @users.before_request
 def assign_request_id():
+    """
+    This function executes before any request.
+    It creates a request uuid and assigns it in the global namespace variable 'g'.
+    """
     g.request_id = request.headers.get("X-Request-ID", str(uuid.uuid4()))
 
 
 @users.after_request
 def add_request_id_to_response(response):
+    """
+    This function executes after any request.
+    It puts the uuid created in the 'g' global variable to the response dictionary.
+    """
     response.headers["X-Request-ID"] = g.request_id
     return response
 
@@ -29,13 +42,17 @@ def add_request_id_to_response(response):
 @users.route("/", methods=["GET"])
 @users.output(schema.GenericResponse, status_code=200)
 def api_get_users():
-    pass
+    """
+    Gets all users.
+    """
 
 
 @users.route("/", methods=["POST"])
 @users.output(schema.GenericResponse, status_code=200)
 def api_post_user():
-    pass
+    """
+    Create a new user.
+    """
 
 
 @users.route("/token", methods=["POST"])
@@ -77,16 +94,22 @@ def api_post_users_token(json: dict):
 @users.route("/<user_id>", methods=["GET"])
 @users.output(schema.GenericResponse, status_code=200)
 def api_get_user():
-    pass
+    """
+    Get an existing user.
+    """
 
 
 @users.route("/<user_id>", methods=["PATCH"])
 @users.output(schema.GenericResponse, status_code=200)
 def api_patch_user():
-    pass
+    """
+    Update an existing user.
+    """
 
 
 @users.route("/<user_id>", methods=["DELETE"])
 @users.output(schema.GenericResponse, status_code=200)
 def api_delete_user():
-    pass
+    """
+    Delete an existing user.
+    """
